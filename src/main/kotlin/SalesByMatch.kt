05bd29f7a5de4f53d27c83f443883c306056d5ca
ckavuzlu <ckavuzlu@gmail.com>
2022-09-24 23:09:02 +0300

    Add `SalesByMatch` solution

---
+++ src/main/kotlin/SalesByMatch.kt
@@ -0,0 +1,53 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+fun sockMerchant(n: Int, ar: Array<Int>): Int {
+    val socksHashMap : HashMap<Int, Int> = HashMap()
+    var totalPair : Int = 0
+    ar.forEach {
+        if(socksHashMap.containsKey(it)){
+            socksHashMap[it] = socksHashMap[it]?.inc()!!
+        }else{
+            socksHashMap[it] = 1
+        }
+    }
+    socksHashMap.forEach { _, numberOfSocks ->
+        totalPair += numberOfSocks/2
+    }
+    return totalPair
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = sockMerchant(n, ar)
+
+    println(result)
+}
