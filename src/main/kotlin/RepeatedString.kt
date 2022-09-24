b5b22a67afb7d60d8c73b88bd481cd317e867ed2
ckavuzlu <ckavuzlu@gmail.com>
2022-09-24 23:06:26 +0300

    Add `RepeatedString` solution

---
+++ src/main/kotlin/RepeatedString.kt
@@ -0,0 +1,54 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+fun repeatedString(s: String, n: Long): Long {
+    var aCount = 0L
+    var lastLetters : Int = (n%s.length).toInt()
+    var tempString = s.take(lastLetters)
+    s.forEach {
+        if(it == 'a') aCount++
+    }
+    aCount *= n/s.length
+
+    tempString.forEach {
+        if(it == 'a') aCount ++
+    }
+
+    return aCount
+
+}
+
+fun main(args: Array<String>) {
+    val s = readLine()!!
+
+    val n = readLine()!!.trim().toLong()
+
+    val result = repeatedString(s, n)
+
+    println(result)
+}
