187e3578e6ce6db564865387136440cf48c61a49
ckavuzlu <ckavuzlu@gmail.com>
2022-09-25 10:48:08 +0300

    Add `TwoStrings` solution

---
+++ src/main/kotlin/TwoStrings.kt
@@ -0,0 +1,54 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'twoStrings' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. STRING s1
+ *  2. STRING s2
+ */
+
+fun twoStrings(s1: String, s2: String): String {
+    var isStringsHaveCommon = "NO"
+
+    s1.toSet().forEach {
+        if (s2.contains(it)){
+            isStringsHaveCommon = "YES"
+            return@forEach
+        }
+    }
+
+    return isStringsHaveCommon
+}
+
+fun main(args: Array<String>) {
+    val q = readLine()!!.trim().toInt()
+
+    for (qItr in 1..q) {
+        val s1 = readLine()!!
+
+        val s2 = readLine()!!
+
+        val result = twoStrings(s1, s2)
+
+        println(result)
+    }
+}
