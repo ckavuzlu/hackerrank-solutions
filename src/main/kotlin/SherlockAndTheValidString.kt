64fcd24a42c012185b42ce43d030f0a297b5fd61
ckavuzlu <ckavuzlu@gmail.com>
2022-11-18 09:29:33 +0300

    Add `SherlockAndTheValidString`solution

---
+++ src/main/kotlin/SherlockAndTheValidString.kt
@@ -0,0 +1,59 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.math.absoluteValue
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+
+I tried to solve as fast as i can. I am sure that there is better way to solve this.
+
+ */
+
+fun isValid(s: String): String {
+    val charMap = HashMap<Char, Int>()
+
+    var changedMoreThanOne = false
+    s.forEach {
+        val newValue = charMap.getOrDefault(it, 0) + 1
+        charMap[it] = newValue
+    }
+    val charCount = charMap[s[0]] ?: 0
+    charMap.forEach {
+        if (charCount != it.value) {
+            if (Math.abs(charCount.minus(it.value)) == 1 || it.value == 1) {
+                if (changedMoreThanOne) {
+                    return "NO"
+                } else {
+                    changedMoreThanOne = true
+                }
+            } else {
+                return "NO"
+            }
+        }
+    }
+    return "YES"
+}
+
+fun main(args: Array<String>) {
+
+    val result = isValid(
+        "ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"
+    )
+
+    println(result)
+}
